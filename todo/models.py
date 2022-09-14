7cd0f8e88a7b78e7157f21f6b357b50496a23efb
fuatsevinc <fuatsevinc@icloud.com>
2022-09-14 13:51:03 +0200

    update

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Todos(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000, blank=True)
+    finished = models.BooleanField(default=True)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+ 
--- todo/views.py
+++ todo/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todos
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "todo/index.html", {"name":"Fuat SEVINC"} )
+    todo_list = Todos.object.all()
+    return render(request, "todo/index.html", {'todo_list':todo_list})
 
 def home(request):
-    return render(request, "todo/home.html", {"namex":"HOME PAGE"} )
+    return render(request, "todo/home.html" )
 
 def about(request):
-    return render(request, "todo/about.html", {"surname":"ABOUT SAYFASI"} )
+    return render(request, "todo/about.html" )
 
 def link(request):
-    return render(request, "todo/link.html", {"tamname":"LINK SAYFASI"} )
+    return render(request, "todo/link.html")
 
 def create(request):
-    return render(request, "todo/create.html", {"pagename":"CRAETA Page"} ) 
+    return render(request, "todo/create.html")
+
