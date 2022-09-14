6b6e9d48844937aebe15fab597377f5033757fdd
fuatsevinc <fuatsevinc@icloud.com>
2022-09-14 23:43:06 +0200

    form created

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from todo.forms import ListForm
 from .models import Todos
-
 
 # Create your views here.
 
 def index(request):
-    todo_list = Todos.object.all()
-    return render(request, "todo/index.html", {'todo_list':todo_list})
+
+
+
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+        if form.is_valid:
+            form.save()
+            todo_list = Todos.objects.all()
+            return render(request, "todo/index.html", {'todo_list':todo_list})
+    else:
+        todo_list = Todos.objects.all()
+        return render(request, "todo/index.html", {'todo_list':todo_list})
 
 def home(request):
     return render(request, "todo/home.html" )
